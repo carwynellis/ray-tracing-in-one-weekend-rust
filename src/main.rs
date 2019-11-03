ea79d03ef66d2531bf355cd2f6baf9e2cdb039b9
Carwyn Ellis <carwynellis@gmail.com>
2019-11-03 14:12:36 +0000

    Initial attempt at a PPM writer
    * need to fix compiler warning about result type and also correct order
    that data is written (seems to be upside down compared to docs)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let nx = 200;
+    let ny = 100;
+
+    let mut file = File::create("image.ppm")?;
+
+    // Write PPM file header.
+    file.write_fmt(format_args!("P3\n{}\n{}\n255\n", nx, ny));
+
+    for y in 0..ny {
+        for x in 0..nx {
+            let r = x as f64 / nx as f64;
+            let g = y as f64 / ny as f64;
+            let b = 0.2;
+            let ir = (255.99 * r) as i64;
+            let ig = (255.99 * g) as i64;
+            let ib = (255.99 * b) as i64;
+            file.write_fmt(format_args!("{} {} {}\n", ir, ig, ib));
+        }
+    }
+
+    Ok(())
 }
