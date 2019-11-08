ff88e3cb12c787d85cf915e47a7775b0c92a6c3a
Carwyn Ellis <carwynellis@gmail.com>
2019-11-08 19:47:42 +0000

    Fix test image render to match example.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> std::io::Result<()> {
     // Write PPM file header.
     file.write_fmt(format_args!("P3\n{}\n{}\n255\n", nx, ny));
 
-    for y in 0..ny {
+    for y in (0..ny).rev() {
         for x in 0..nx {
             let r = x as f64 / nx as f64;
             let g = y as f64 / ny as f64;
